342d0293dc61f14774f9b29e900cb40e9e860e0b
VMang0 <valeriakorolkova2703@gmail.com>
2023-04-17 23:22:50 +0300

    pagination for room catalog

--- src/pages/manager/rooms/all_rooms/AllRooms.js
+++ src/pages/manager/rooms/all_rooms/AllRooms.js
@@ -1,17 +1,29 @@
-import React, {useEffect, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import axios from "axios";
 import "./AllRooms.css"
 import {Link, useParams} from "react-router-dom";
 import {BiArea} from "react-icons/bi";
 import {IoBedOutline} from "react-icons/io5";
 import {SlPeople} from "react-icons/sl";
-import noFoto from "../../../../css_all/img/nophoto.png"
+import noFoto from "../../../../css_all/img/nophoto.png";
+import {MdDeleteOutline, MdOutlineEdit} from "react-icons/md";
+import BackFon from "../../../admin/control_managers/backfon/BackFon";
+import {AuthContext} from "../../../../contexts/authContext";
+import useTable from "../../../admin/table/useTable";
+import TableFooter from "../../../admin/table/TableFooter";
 export default function AllRooms(){
 
     const [rooms, setRoom] = useState([]);
-    const [images, setImages] = useState([]);
     const [firstRoomImage, setFirstRoomImage] = useState({});
-    const { id } = useParams();
+    const {authData, setAuthData} = useContext(AuthContext);
+
+    const [showModal, setShowModal] = useState(false);
+    const [selectedRoom, setSelectedRoom] = useState(null);
+    const [showBackFon, setShowBackFon] = useState(false);
+
+    const [page, setPage] = useState(1);
+    const { slice, range } = useTable(rooms, page, 6);
+
     const LoadRooms = async ()=> {
         const result = await axios.get("http://localhost:8080/list/rooms");
         setRoom(result.data);
@@ -41,29 +53,53 @@ export default function AllRooms(){
         updateFirstRoomImage();
     }, [rooms]);
 
+    const deleteRoom = async (id) => {
+        setSelectedRoom(id);
+        setShowModal(true);
+        setShowBackFon(true);
+    };
+
+    const handleConfirmDelete = async () => {
+        await axios.delete(`http://localhost:8080/room/${selectedRoom}`);
+        setSelectedRoom(null);
+        setShowModal(false);
+        setShowBackFon(false);
+        LoadRooms();
+    };
+
+    const handleCancelDelete = () => {
+        setSelectedRoom(null);
+        setShowModal(false);
+        setShowBackFon(false);
+    };
+
+
     return(
         <div className="main-cards-container">
+            {showBackFon && <BackFon show={showBackFon} clicked={handleCancelDelete}></BackFon>}
             <div className="name-page"><span>Номера и апартаменты</span></div>
             <div className="cards-container">
                 <div className="cards-container2">
                 {
-                    rooms.map((room) =>(
+                    slice.map((room) =>(
                         <div className="room-card-grid" key={room.id}>
                             <div className="room-card">
                                 <div className="image_container">
-                                    {/*{firstRoomImage[room.id] && (
-                                        <img className="image" src={firstRoomImage[room.id]} alt="room" />
-                                    )}*/}
                                     {
                                         firstRoomImage[room.id] ? (
-                                            <img className="image" src={firstRoomImage[room.id]} alt="room" />
+                                            <img className="image_room" src={firstRoomImage[room.id]} alt="preview room image" width="400px" height="300px" loading="lazy"/>
                                         ):(
-                                            <img className="image" src={noFoto} alt="room" />
+                                            <img className="image_room" src={noFoto} alt="no preview room image" width="400px" height="300px" loading="lazy"/>
                                         )
                                     }
-                                    <div>
-
-                                    </div>
+                                    {
+                                        authData && authData.roles[0] === 'MANAGER' ? (
+                                            <div className="manager-tools">
+                                                <button className="manager-tools-btn"><MdOutlineEdit/></button>
+                                                <button className="manager-tools-btn" onClick={()=> deleteRoom(room.id)}><MdDeleteOutline/></button>
+                                            </div>
+                                        ): ""
+                                    }
                                 </div>
                                 <div className="card-room-info">
                                     <div className="name-room">{room.name}</div>
@@ -82,6 +118,22 @@ export default function AllRooms(){
                 }
                 </div>
             </div>
+            <TableFooter range={range} slice={slice} setPage={setPage} page={page} />
+            {showModal && (
+                <div className="modal_window">
+                    <div className="modal_main">
+                        <p className="question">Вы уверены, что хотите удалить данный номер?</p>
+                        <div className="modal_buttons">
+                            <button className="btn btn-secondary" onClick={handleCancelDelete}>
+                                Cancel
+                            </button>
+                            <button className="btn btn-danger" onClick={handleConfirmDelete}>
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
