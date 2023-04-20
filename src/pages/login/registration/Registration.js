13f65695566bfddb9cb7694b0fe9417a4a9ee24b
VMang0 <valeriakorolkova2703@gmail.com>
2023-04-20 23:25:47 +0300

    registration

--- src/pages/login/registration/Registration.js
+++ src/pages/login/registration/Registration.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
-
+import {FaGithubAlt, FaInstagram, FaTelegramPlane, FaTwitter} from "react-icons/fa";
+import "./Registration.css"
 export default function Registration() {
     let navigate = useNavigate();
     const [user, setUser] = useState({
@@ -56,13 +57,12 @@ export default function Registration() {
 
         const verifyCode = async () => {
             if (!verificationCode) {
-                alert('Please enter verification code.');
+               alert('Please enter verification code.');
                 return;
             }
             try {
                 const response = await axios.post('http://localhost:8080/verify-code', { email, verificationCode });
                 if (response.status === 200) {
-                    alert('Verification successful!');
                     setVer_success(true);
                 }
             } catch (error) {
@@ -72,38 +72,82 @@ export default function Registration() {
             }
         };
 
+    useEffect(() =>{
+        const input = document.querySelector('.form_login');
+        const label = document.querySelector('.label-input');
+        const input_pass = document.querySelector('.form_login_pass');
+        const label_pass = document.querySelector('.label-input-pass');
+        const var_input = document.querySelector('.ver_code_input');
+
+        input.addEventListener('input', function() {
+            if (input.value) {
+                label.classList.add('filled');
+            } else {
+                label.classList.remove('filled');
+            }
+        });
+        input_pass.addEventListener('input', function() {
+            if (input_pass.value) {
+                label_pass.classList.add('filled');
+            } else {
+                label_pass.classList.remove('filled');
+            }
+        });
+
+        var_input?.addEventListener('input',  ()=> {
+            if (!var_input.value) {
+                var_input.classList.add('error');
+            } else {
+                var_input.classList.remove('error');
+            }
+
+        });
+
+    }, []);
+
         return (
-            <div>
-                <h2>Register</h2>
-                <form onSubmit={onSubmit}>
-                    <div>
-                        <label>Email:</label>
-                        <input type='email' name='email' value={email} onChange={onInputChange} />
-                    </div>
-                    <div>
-                        <label>Password:</label>
-                        <input type='password' name='password' value={password} onChange={onInputChange} />
-                    </div>
-                    {codeSent ? (
-                        <div>
-                            <label>Verification Code:</label>
-                            <input type='text' name='verificationCode' value={verificationCode} onChange={(e) => setVerificationCode(e.target.value)} />
-                            <button type='button' onClick={verifyCode}>
-                                Verify
-                            </button>
+            <div className= "main_container_page">
+                <div className= "window login">
+                    <form onSubmit={onSubmit}>
+                        <div className="btn-navigate">
+                            <div className="sign_in reg_page_sign_in"><Link to="/loginuser">Sign in</Link></div>
+                            <div className="sign_up reg_page_sign_up" ><Link to="/registration">Register</Link></div>
                         </div>
-                    ) : (
-                        <button type='button' onClick={sendVerificationCode}>
-                            Get Verification Code
-                        </button>
-                    )}
+                        <div className="form-for-icon-login">
+                            <ul className="list_icon_login">
+                                <li><button className="icon_btn_login instagram_login" ><FaInstagram/></button></li>
+                                <li><button className="icon_btn_login github_login" ><FaGithubAlt/></button></li>
+                                <li><button className="icon_btn_login telegram_login"><FaTelegramPlane /></button></li>
+                                <li><button className="icon_btn_login telegram_login"><FaTwitter/></button></li>
+                            </ul>
+                        </div>
+                        <div className="input-form">
+                            <div className="input-login">
+                                <input className= "form_login" type='email' name='email' value={email} onChange={onInputChange} placeholder="Email"/>
+                                <label className="label-input">Email</label>
+                            </div>
+                            <div className="input-login">
+                                <input className= "form_login_pass" type='password' name='password' value={password} onChange={onInputChange} placeholder="Password" minLength="4" maxLength="20"/>
+                                <label className="label-input-pass">Password</label>
+                            </div>
+                        </div>
+                        {codeSent ? (
+                            <div>
+                                {!ver_success ? (
+                                    <div className="input-login">
+                                        <input className="ver_code_input" placeholder="Verification code" type='text' name='verificationCode' value={verificationCode} onChange={(e) => setVerificationCode(e.target.value)}/>
+                                        <button className="btn-reg" type='button' onClick={verifyCode}>Verify</button>
+                                    </div> )
+                                        : (<button className="btn-reg" type='submit'>Register</button>)}
+                            </div>
+                        ) : (
+                            <div className="form-for-getcode">
+                                <Link className="link_verification" type='button' onClick={sendVerificationCode}>Get Verification Code</Link>
+                            </div>
+                        )}
 
-                    {ver_success ? <button type='submit'>Register</button> : ""}
-
-                </form>
-                <p>
-                    Already have an account? <Link to='/login'>Login</Link>
-                </p>
+                    </form>
+                </div>
             </div>
         );
     }
