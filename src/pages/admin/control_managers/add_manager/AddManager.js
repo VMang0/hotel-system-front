fd2ee5032a66c4571b80058b9a69449bf6ceec6a
VMang0 <valeriakorolkova2703@gmail.com>
2023-04-20 23:22:08 +0300

    add manager

--- src/pages/admin/control_managers/add_manager/AddManager.js
+++ src/pages/admin/control_managers/add_manager/AddManager.js
@@ -16,29 +16,33 @@ export default function AddManager() {
 
     const onSubmit = async (e) =>{
         e.preventDefault();
-        await axios.post("http://localhost:8080/add_manager", user)
-        navigate("/admin/dashboard-manager");
+        try {
+            const response = await axios.post("http://localhost:8080/add_manager", user)
+            if(response.status === 200){
+                navigate("/admin/dashboard-manager");
+            }
+        }catch (error){
+            alert('Ошибка при добавлении менеджера. Пользователь с таким email уже существует!');
+            console.error(error);
+        }
     };
     return (
         <div className= "container-add-manager">
             <div className= "container-add-manager-window">
                 <div className= "form-add">
-                    <h3 className= "text-center m-3 ">Register Manager</h3>
                     <form onSubmit={(e) => onSubmit(e)}>
-
                         <div className="mb-3">
                             <label htmlFor="Email" className="form-label">Email</label>
-                            <input type= {"text"} className= "form-control" placeholder= "Enter your email" name = "email"
-                                   value={email} onChange={(e)=> onInputChange(e)}/>
+                            <input type= "email" className= "form-control" placeholder= "Enter your email" name = "email"
+                                   value={email} onChange={(e)=> onInputChange(e)} required/>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="Password" className="form-label">Password</label>
-                            <input type= {"text"} className= "form-control" placeholder= "Enter your password" name = "password"
-                                   value={password} onChange={(e)=> onInputChange(e)}/>
+                            <input type= "password" className= "form-control" placeholder= "Enter your password" name = "password"
+                                   value={password} onChange={(e)=> onInputChange(e)} required minLength="4" maxLength="20"/>
                         </div>
                         <button type="submit" className= "btn btn-outline-primary">Submit</button>
                         <Link  className= "btn btn-danger mx-2" to = {"/admin/dashboard-manager"}>Cancel</Link>
-
                     </form>
                 </div>
             </div>
