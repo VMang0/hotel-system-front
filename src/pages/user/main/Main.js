b604a2f14ad74d06bd26dc422c846ff6c0040f36
VMang0 <valeriakorolkova2703@gmail.com>
2023-04-20 23:26:37 +0300

    main page

--- src/pages/user/main/Main.js
+++ src/pages/user/main/Main.js
@@ -3,8 +3,8 @@ import image from "../../../css_all/img/e074bd149264809.62e3fc9247a02.jpg";
 import "./Main.css"
 export function Main() {
     return (
-        <div className="container">
-           <img src={image}/>
+        <div className="container-home">
+           <img src={image} className="main_image" alt="main image"/>
         </div>
     )
 }
