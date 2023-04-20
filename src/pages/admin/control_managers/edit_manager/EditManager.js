250793203f40a531e1784614fc2a3aeaeae72f15
VMang0 <valeriakorolkova2703@gmail.com>
2023-04-20 23:26:03 +0300

    edit manager

--- src/pages/admin/control_managers/edit_manager/EditManager.js
+++ src/pages/admin/control_managers/edit_manager/EditManager.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import axios from "axios";
 import {Link, useNavigate, useParams} from "react-router-dom";
+import "./EditManager.css"
 
 export default function EditManager() {
     let navigate = useNavigate();
@@ -15,8 +16,7 @@ export default function EditManager() {
     const { email , roles} = user;
 
     const onInputChange = (e) => {
-        /*setUser({ ...user, [e.target.name]: e.target.value });*/
-        setUser({ ...user, roles: [e.target.value] })
+        setUser({ ...user, [e.target.name]: e.target.value });
     };
 
     useEffect(() => {
@@ -25,25 +25,26 @@ export default function EditManager() {
 
     const onSubmit = async (e) => {
         e.preventDefault();
-        await axios.put(`http://localhost:8080/user/${id}`, user);
-        navigate("/");
+        await axios.put(`http://localhost:8080/user/${id}`, {...user, roles: [user.roles]});
+        navigate("/admin/dashboard-manager");
     };
 
     const loadUser = async () => {
         const result = await axios.get(`http://localhost:8080/user/${id}`);
-        setUser(result.data);
+        const userRoles = result.data.roles.join(',');
+        setUser({ ...result.data, roles: userRoles });
     };
 
     return (
-        <div className= "container">
-            <div className= "row">
-                <div className= "col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+        <div className= "container-edit-manager">
+            <div className= "container-edit-manager-window">
+                <div className= "form-edit">
                     <h3 className= "text-center m-3 ">Edit User</h3>
                     <form onSubmit={(e) => onSubmit(e)}>
-                        <div className="mb-3">
-                            <label htmlFor="Email" className="form-label">Email</label>
-                            <input type= {"text"} className= "form-control" placeholder= "Enter your email" name = "email"
-                                   value={email} onChange={(e)=> onInputChange(e)}/>
+                        <div className="input-container">
+                            <input type= "email" className= "form-control" placeholder="Email" name = "email"
+                                   value={email} onChange={(e)=> onInputChange(e)} required/>
+                            <label className="form-label">Email</label>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="Role" className="form-label"> Role </label>
@@ -53,8 +54,7 @@ export default function EditManager() {
                             </select>
                         </div>
                         <button type="submit" className= "btn btn-outline-primary">Submit</button>
-                        <Link  className= "btn btn-danger mx-2" to = "/">Cancel</Link>
-
+                        <Link  className= "btn btn-danger mx-2" to = "/admin/dashboard-manager">Cancel</Link>
                     </form>
                 </div>
             </div>
