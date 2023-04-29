e67e73a0fb3253169b6cf43cc8beaceb0efd43de
VMang0 <valeriakorolkova2703@gmail.com>
2023-04-29 22:04:36 +0300

    add room, css, validation

--- src/pages/manager/rooms/add_rooms/AddRoom.js
+++ src/pages/manager/rooms/add_rooms/AddRoom.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import './AddRoom.css';
 import {useNavigate} from "react-router-dom";
 import Dropzone, { useDropzone } from "react-dropzone";
+import {toast} from "react-toastify";
+import {TiDeleteOutline} from "react-icons/ti"
 export default function AddRoom() {
     let navigate = useNavigate();
     const [typeRoom, setTypeRoom] = useState([]);
@@ -50,13 +52,18 @@ export default function AddRoom() {
             formData.append('files', new File([""], "no-photo.jpg", { type: "image/jpeg" }));
         }
 
-        await axios.post("http://localhost:8080/add_room", formData,
-            {
-                headers: {
-                    "Content-Type": "multipart/form-data",
-                },
-            })
-        navigate("/");
+        if(type_room === "" || type_bed === "" || number === ""){
+            toast.error('Заполните все поля!');
+        }else{
+            await axios.post("http://localhost:8080/add_room", formData,
+                {
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                    },
+                })
+            navigate("/");
+            toast.success('Вы успешно добавили новый номер!');
+        }
 
     };
 
@@ -70,55 +77,103 @@ export default function AddRoom() {
         setRoom({...room,[e.target.name]: e.target.value});
     };
 
-    return(
-        <form onSubmit={(e) => onSubmit(e)} encType="multipart/form-data">
+    const removeFile = (index) => {
+        const newFiles = [...files];
+        newFiles.splice(index, 1);
+        setFiles(newFiles);
+    };
 
-            <div className="form-create">
-                <select value={type_room} onChange={(e) => onInputChange(e)} name="type_room">
-                    <option key={typeRoom.id} value={typeRoom.name}>{typeRoom.name}</option>
-                    {
-                        typeRoom.map(typeRoom =>(
-                            <option key={typeRoom.id} value={typeRoom.name}>{typeRoom.name}</option>
-                        ))
-                    }
-                </select>
-                <select value={type_bed} onChange={(e) => onInputChange(e)} name="type_bed">
-                    <option value="" disabled>Choose an option</option>
-                    {
-                        typeBed.map(typeBed =>(
-                            <option key={typeBed.id} value={typeBed.name}>{typeBed.name}</option>
-                        ))
-                    }
-                </select>
-                <select value={number} onChange={(e) => onInputChange(e)} name="number">
-                    <option value="" disabled>Choose an option</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </select>
-                <input type="number" value={cost} placeholder="цена" onChange={(e) => onInputChange(e)} name="cost"/>
-                <input type="text" value={description} placeholder="текст" onChange={(e) => onInputChange(e)} name="description"/>
-                <input type="number" value={square} placeholder="площадь" onChange={(e) => onInputChange(e)} name="square"/>
-                <input type="text" value={name} placeholder="название" onChange={(e) => onInputChange(e)} name="name"/>
-            </div>
-            <Dropzone onDrop={onDrop}>
-                {({getRootProps, getInputProps}) => (
-                    <div {...getRootProps()}>
-                        <input {...getInputProps({name: 'files'})} style={{color: "#fff"}} />
-                        <p>Drag and drop some files here, or click to select files</p>
-                        <div className="file-list">
-                            {files.map((file) => (
-                                <p key={file.name}>{file.name}</p>
-                            ))}
+    return(
+        <div>
+            <div className="name-page"><span>Добавление номера</span></div>
+            <div className="form-add-room">
+                <form onSubmit={(e) => onSubmit(e)} encType="multipart/form-data">
+                    <div className="form-create">
+                        <div className="form-create-ones">
+                            <div className="text-add-room"><label>Тип номера</label></div>
+                            <select value={type_room} className="select-add-room" onChange={(e) => onInputChange(e)} name="type_room" required>
+                                <option>Choose</option>
+                                {
+                                    typeRoom.map(typeRoom =>(
+                                        <option key={typeRoom.id} value={typeRoom.name}>{typeRoom.name}</option>
+                                    ))
+                                }
+                            </select>
+                        </div>
+                       <div className="form-create-ones">
+                           <div className="text-add-room"><label>Тип кровати</label></div>
+                           <select value={type_bed} className="select-add-room" onChange={(e) => onInputChange(e)} name="type_bed" required>
+                               <option>Choose</option>
+                               {
+                                   typeBed.map(typeBed =>(
+                                       <option key={typeBed.id} value={typeBed.name}>{typeBed.name}</option>
+                                   ))
+                               }
+                           </select>
+                       </div>
+                        <div className="form-create-ones">
+                            <div className="text-add-room"><label>Максимальное кол-во человек</label></div>
+                            <select value={number} className="select-add-room" onChange={(e) => onInputChange(e)} name="number" required>
+                                <option>Choose</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select>
+                        </div>
+                       <div className="form-create-ones">
+                           <div className="text-add-room"><label>Стоимость на 1-го</label></div>
+                           <input type="number" value={cost} placeholder="цена" onChange={(e) => onInputChange(e)} name="cost" required/>
+                       </div>
+                       <div className="form-create-ones">
+                           <div className="text-add-room"><label>Описание</label></div>
+                           <textarea maxLength="380" value={description} placeholder="текст" onChange={(e) => onInputChange(e)} name="description" required/>
+                       </div>
+                        <div className="form-create-ones">
+                            <div className="text-add-room"><label>Площадь</label></div>
+                            <input type="number" value={square} placeholder="площадь" onChange={(e) => onInputChange(e)} name="square" required/>
+                        </div>
+                        <div className="form-create-ones">
+                            <div className="text-add-room"><label>Название</label></div>
+                            <input maxLength="67" type="text" value={name} placeholder="название" onChange={(e) => onInputChange(e)} name="name" required/>
                         </div>
                     </div>
+                    {/*<Dropzone onDrop={onDrop}>
+                        {({getRootProps, getInputProps}) => (
+                            <div {...getRootProps()}>
+                                <input {...getInputProps({name: 'files'})} style={{color: "#fff"}} className="dropfiles"/>
+                                <div className="file-list">
+                                    {files.map((file) => (
+                                        <p key={file.name}>{file.name}</p>
+                                    ))}
+                                </div>
+                            </div>
 
-                )}
-            </Dropzone>
-            <button type="submit" className= "btn btn-outline-primary">Submit</button>
-        </form>
-
+                        )}
+                    </Dropzone>*/}
+                    <div className="form-create-ones">
+                        <div className="text-add-room"><label>Фотографии</label></div>
+                        <div {...getRootProps()} className="dropzone">
+                            <input {...getInputProps()} />
+                            <p>Перетащите файлы сюда или кликните, чтобы выбрать файлы</p>
+                        </div>
+                        {files.length > 0 && (
+                            <ul>
+                                {files.map((file, index) => (
+                                    <li key={index}>
+                                        <div className="form-for-img">
+                                            <img src={URL.createObjectURL(file)} alt={file.name} width="100px"/>
+                                            <button onClick={() => removeFile(index)} className="btn-delete-img"><TiDeleteOutline/></button>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </div>
+                    <button type="submit" className= "btn-add-room">Добавить</button>
+                </form>
+            </div>
+        </div>
     );
 }
